6d05ac4b167e8983786bb9a8ff2cde99acf6d17d
Nicole Rojas <nicole.eli.valdes@gmail.com>
2024-02-22 13:51:34 +0000

    Model settings

--- codestar/settings.py
+++ codestar/settings.py
@@ -91,11 +91,11 @@ DATABASES = {
         default=os.environ.get("postgres://jjmgkggm:KMm-LEj4xD-vCQJn39VXfPx8xU-okabe@trumpet.db.elephantsql.com/jjmgkggm"))
 }
 
-CRS_Trusted_origins = ["https://8000-nicolelir-djangoblog-h2vur9sk729.ws-eu108.gitpod.io"]
+
 CSRF_TRUSTED_ORIGINS = [
     "https://*.codeanyapp.com",
     "https://*.herokuapp.com",
-    "https://8000-nicolelir-djangoblog-h2vur9sk729.ws-eu108.gitpod.io"
+    "https://8000-nicolelir-djangoblog-h2vur9sk729.ws-eu108.gitpod.io",'.herokuapp.com'
 ]
 
 # Password validation
