6d5e2119489e35be4aafc09bfb33130d7914f051
Nicole Rojas <nicole.eli.valdes@gmail.com>
2024-02-22 22:23:26 +0000

    class meta settings

--- blog/models.py
+++ blog/models.py
@@ -26,7 +26,7 @@ class Comment(models.Model):
     created_on = models.DateTimeField(auto_now_add=True)
 
 class Meta:
-        ordering = ["-created_on"]
+        ordering = ["created_on"]
 
         def __str__(self):
-          return f"The title of this post is {self.title}"
+          return f"Comment {self.body} by {self.author}"
