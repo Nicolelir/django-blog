91d00a2bcd7edb880a6f3952b02649b12e6c6405
Nicole Rojas <nicole.eli.valdes@gmail.com>
2024-02-23 20:50:42 +0000

    Add filter and list display to  admin dashboard

--- blog/models.py
+++ blog/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+# Create your models here.
 STATUS = ((0, "Draft"), (1, "Published"))
 
-# Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
@@ -12,9 +12,14 @@ class Post(models.Model):
 )
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
-    status = models.IntegerField(choices=STATUS, default=0)
     excerpt = models.TextField(blank=True)
+    
+    status = models.IntegerField(choices=STATUS, default=0)
     updated_on = models.DateTimeField(auto_now=True)
+    class Meta:
+        ordering = ["-created_on"]
+    def __str__(self):
+        return f"{self.title} | written by {self.author}"
 
 class Comment(models.Model):
     post = models.ForeignKey(
@@ -24,9 +29,8 @@ class Comment(models.Model):
     body = models.TextField()
     approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
-
     class Meta:
-        ordering = ["-created_on"]
+        ordering = ["created_on"]
 
     def __str__(self):
         return f"Comment {self.body} by {self.author}"
