8f08487ebda966a2cea3290eb9ad60722213f632
Nicole Rojas <nicole.eli.valdes@gmail.com>
2024-02-28 11:16:00 +0000

    Add images to posts

--- codestar/settings.py
+++ codestar/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY=os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG=False
 
-ALLOWED_HOSTS = ['8000-nicolelir-djangoblog-eqwhv9rsgwm.ws-eu108.gitpod.io','.herokuapp.com']
+ALLOWED_HOSTS = ['8000-nicolelir-djangoblog-mjp3ca2lzvo.ws-eu108.gitpod.io','.herokuapp.com']
 
 
 # Application definition
